c2d64f52689f3771ebab8086f7f8bf7496f5c0d6
Valeriy V Nepluev <greenet2007@gmail.com>
2021-05-24 01:22:05 +0300

    feat: add menu and commands

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const bot = new botApi(token, { polling: true })
 const menuOptions = {
     reply_markup: JSON.stringify({
         inline_keyboard: [
-            [{ text: 'О яхтах', callback_data: '/about' }],
-            [{ text: 'Как нас найти', callback_data: '/map' }],
+            [{ text: 'Прогулки на яхте', callback_data: '/sell' },
+             { text: 'Фотосессии', callback_data: '/photo' }],
             [{ text: 'Туристические маршруты', callback_data: '/tour' }],
-            [{ text: 'Аренда яхт', callback_data: '/sell' }],
-            [{ text: 'Фотосессии', callback_data: '/photo' }]
+            [{ text: 'О нас', callback_data: '/about' },
+             { text: 'Как добраться', callback_data: '/map' }]
         ]
     })
 }
@@ -19,7 +19,11 @@ const start = () => {
     // список команд бота
     bot.setMyCommands([
         { command: '/start', description: 'Приветствие' },
-        { command: '/stop', description: 'Приветствие' }
+        { command: '/about', description: 'О нас' },
+        { command: '/sell', description: 'Прогулки на яхте' },
+        { command: '/photo', description: 'Фотосессии' },
+        { command: '/tour', description: 'Туристические маршруты' },
+        { command: '/map', description: 'Как добраться' }
     ])
 
     // сообщения для бота
@@ -29,14 +33,12 @@ const start = () => {
 
         if (text === '/start') {
             await bot.sendSticker(chatID, 'https://tlgrm.ru/_/stickers/1d4/336/1d433610-907e-31e1-bcd1-bb87fb42375f/2.webp')
-            return bot.sendMessage(chatID, 'Старт', menuOptions) // кнопки начального меню
-            //return bot.sendMessage(chatID, `Приветствую ${msg.from.first_name}! Сдесь вы можете узнать об аренде парусных яхт и ознакомиться с вариантами прогулок.`)
+            return bot.sendMessage(chatID, `Приветствую ${msg.from.first_name}! Сдесь вы можете узнать об аренде парусных яхт и ознакомиться с вариантами прогулок.`, menuOptions) // кнопки начального меню
         }
 
         // сообщение по умолчанию
         return bot.sendMessage(chatID, `Я нахожусь в стадии разработки и еще мало что умею.`)
 
-    console.log(msg);
     })
 
     // когда кнопку меню нажали
