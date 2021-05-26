450f99f3c023e3bcf71be088462adac3e4191143
Valeriy V Nepluev <greenet2007@gmail.com>
2021-05-27 00:04:07 +0300

    feat: add connect sequalize

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # sofia_bot
+
 Telegram bot rent yacht
+
+### For DEV
+
+```
+npm run dev
+```
+
+### For PRODUCTION
+
+```
+npm run start
+```
+
+### Packeges
+
+```
+npm init -y
+npm i node-telegram-bot-api
+npm i nodemon
+npm install --save sequelize
+npm install --save mariadb
+```
--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const token = ''
+
+// подключаем API бота telegram
 const botApi = require('node-telegram-bot-api')
 const bot = new botApi(token, { polling: true })
+
+// подключаем БД
+const { Sequelize } = require('sequelize')
+const { DataTypes } = require('sequelize')
+const sequalize = new Sequelize(
+    'val_',
+    'val_',
+    'vD',
+    {
+        host: '00.0.00.00',
+        dialect: 'mariadb'
+    })
+const UserModel = sequalize.define('user', {
+    ID: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
+    FROM_ID: { type: DataTypes.TEXT, unique: true }
+})
+
 
 // Кнопки основного меню
 const menuOptions = {
@@ -15,10 +34,18 @@ const menuOptions = {
     })
 }
 
-const start = () => {
+const start = async () => {
+    // Подключаемся к БД
+    try {
+        await sequalize.authenticate()
+        await sequalize.sync()
+    } catch (error) {
+        console.log('Подключение к БД сломалось', error);
+    }
+
     // список команд бота
     bot.setMyCommands([
-        { command: '/start', description: 'Приветствие' },
+        { command: '/start', description: 'Главное меню' },
         { command: '/about', description: 'О нас' },
         { command: '/sell', description: 'Прогулки на яхте' },
         { command: '/photo', description: 'Фотосессии' },
@@ -31,25 +58,39 @@ const start = () => {
         const text = msg.text
         const chatID = msg.chat.id
 
-        if (text === '/start') {
-            await bot.sendSticker(chatID, 'https://tlgrm.ru/_/stickers/1d4/336/1d433610-907e-31e1-bcd1-bb87fb42375f/2.webp')
-            return bot.sendMessage(chatID, `Приветствую ${msg.from.first_name}! Сдесь вы можете узнать об аренде парусных яхт и ознакомиться с вариантами прогулок.`, menuOptions) // кнопки начального меню
+        try {
+            console.log(msg);
+            
+            if (text === '/start') {
+                await UserModel.create({chatID})
+                await bot.sendSticker(chatID, 'https://tlgrm.ru/_/stickers/1d4/336/1d433610-907e-31e1-bcd1-bb87fb42375f/2.webp')
+                return await bot.sendMessage(chatID, `Приветствую ${msg.from.first_name}! Информация об аренде парусных яхт и варианты туристических прогулок.`, menuOptions) // кнопки начального меню
+            }
+    
+            // сообщение по умолчанию
+            return await bot.sendMessage(chatID, `Я нахожусь в стадии разработки и еще мало что умею.`)
+        } catch (error) {
+            return bot.sendMessage(chatID, `Упс, в боте какая-то ошибочка!`)
         }
-
-        // сообщение по умолчанию
-        return bot.sendMessage(chatID, `Я нахожусь в стадии разработки и еще мало что умею.`)
-
+        
     })
 
     // когда кнопку меню нажали
-    bot.on('callback_query', msg => {
+    bot.on('callback_query', async msg => {
         const data = msg.data
         const chatID = msg.message.chat.id
 
-        bot.sendMessage(chatID, `Ты нажал кнопку ${data}`)
+        await bot.sendMessage(chatID, `Ты нажал кнопку ${data}`)
 
         console.log(msg);
     })
 }
+
+// пробуем коннектится к базе
+//mysqlConnection.connect(err => {
+//    if (err) {
+//        console.log(err);
+//    }
+//})
 
 start()
